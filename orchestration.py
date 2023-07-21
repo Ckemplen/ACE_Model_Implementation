da72050048483a615cc52fa7ba33f8326ef7dab3
sevenChrisK <chris@plant-parenthood.co.uk>
2023-07-21 12:54:15 +0100

    first docstring

---
+++ orchestration.py
@@ -0,0 +1,12 @@
+"""
+Brings together the various layers to act in concert.
+
+Layers:
+
+- 1 Aspirational Layer: Mission, values, purpose, ethics, vision, morals
+- 2 Global Strategy: Long term thinking, context, like a CEO
+- 3 Agent Model: (Self) Capabilities, configuration, learning
+- 4 Executive Function: Planning, forecasting, directives, resources
+- 5 Cognitive Control: Task switching & selection, frustration, damping
+- 6 Task Prosecution: One task at a time, detect success & failure
+"""
